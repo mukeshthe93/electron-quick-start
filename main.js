32517fb7017fb3cec29e57a84eb5fb95fb9331ba
mukeshthe93 <mukeshthe93@gmail.com>
2019-05-07 16:46:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const nativeImage = require('electron').nativeImage
+const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
